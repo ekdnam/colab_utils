940ffde3e550106f63e6c9848fbd688089d31b2c
Aditya Mandke <40426312+ekdnam@users.noreply.github.com>
2021-07-04 21:34:53 +0530

    Update download_and_move.sh

--- sockeye/multilingual/download_and_move.sh
+++ sockeye/multilingual/download_and_move.sh
@@ -1,2 +1,7 @@
 gdown --id 12ycYSzLIG253AFN35Y6qoyf9wtkOjakp
 tar -xzvf /content/2017-01-trnmted.tgz
+rm -rf /content/2017-01-trnmted.tgz
+mv /content/2017-01-trnmted/texts/DeEnItNlRo/DeEnItNlRo/DeEnItNlRo-DeEnItNlRo.tgz /content/DeEnItNlRo-DeEnItNlRo.tgz
+rm -rf /content/2017-01-trnmted
+mkdir -p iwslt17_orig data
+mv /content/DeEnItNlRo-DeEnItNlRo.tgz /content/iwslt17_orig/DeEnItNlRo-DeEnItNlRo.tgz
